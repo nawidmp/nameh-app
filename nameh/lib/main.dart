a327a525cadb4a95056180ad162472f6aaefdaea
Nawid <namehcompany@gmail.com>
2024-03-01 01:39:25 +0430

    gggg

--- nameh/lib/main.dart
+++ nameh/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         // This is the theme of your application.
 
-        //   ---------------------------------- fuck you mahmood ---------------------
+        //   --------------------------------- fuck you mahmood ---------------------
 
         // TRY THIS: Try running your application with "flutter run". You'll see
         // the application has a blue toolbar. Then, without quitting the app,
